ff5849b6ece4ad5a2c12f2da98179839a1528c88
huolongteng <hlt@bistu.edu.cn>
2025-09-25 14:33:42 +0800

    Test Qwen2 VL model's performance in other tasks.

---
+++ multi_images_inference.py
@@ -0,0 +1,44 @@
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+
+# default: Load the model on the available device(s)
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    "Qwen2-VL-2B-Instruct", torch_dtype="auto", device_map="auto"
+)
+
+processor = AutoProcessor.from_pretrained("Qwen2-VL-2B-Instruct")
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": "images/profile.png"},
+            {"type": "image", "image": "images/tbg.jpg"},
+            {"type": "text", "text": "Identify the similarities between these images."},
+        ],
+    }
+]
+
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+image_inputs, video_inputs = process_vision_info(messages)
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to("cuda")
+
+# Inference
+generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
